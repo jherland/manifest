d17cb63ffce282892cdddba09352b81d92002ed6
Johan Herland <johan@herland.net>
2013-12-08 11:53:57 +0100

    Add Manifest.write() for writing Manifest objects back to file

--- manifest.py
+++ manifest.py
@@ -77,3 +77,9 @@ class Manifest(dict):
 
     def setparent(self, manifest):
         self._parent = weakref.ref(manifest) if manifest is not None else None
+
+    def write(self, f, level = 0, indent = "\t"):
+        """Write this Manifest in parse()able text format to the given file."""
+        for name, m in sorted(self.iteritems()):
+            print >>f, indent * level + name
+            m.write(f, level + 1, indent)
--- test.py
+++ test.py
@@ -150,5 +150,36 @@ class TestManifest_parse(unittest.TestCase):
         self.assertEqual(m["foo"].getparent(), m)
         self.assertEqual(m["foo"]["bar"].getparent(), m["foo"])
 
+class TestManifest_write(unittest.TestCase):
+
+    def must_equal(self, lines, expect, *args, **kwargs):
+        m = Manifest.parse(lines)
+        s = StringIO()
+        m.write(s, *args, **kwargs)
+        self.assertEqual(s.getvalue(), expect)
+
+    def test_empty(self):
+        self.must_equal("", "")
+
+    def test_single_entry(self):
+        self.must_equal(["foo"], "foo\n")
+
+    def test_multiple_entries_are_sorted(self):
+        self.must_equal(["foo", "bar", "baz"], "bar\nbaz\nfoo\n")
+
+    def test_entry_with_child(self):
+        self.must_equal(["foo", "\tbar"], "foo\n\tbar\n")
+
+    def test_entry_with_grandchild(self):
+        self.must_equal(["foo", "\tbar", "\t\tbaz"], "foo\n\tbar\n\t\tbaz\n")
+
+    def test_supply_indent(self):
+        self.must_equal(["foo", "\tbar", "\t\tbaz"], "foo\n--bar\n----baz\n",
+                        indent = "--")
+
+    def test_supply_level(self):
+        self.must_equal(["foo", "\tbar", "\t\tbaz"],
+                        "XXXfoo\nXXXXbar\nXXXXXbaz\n", indent = "X", level = 3)
+
 if __name__ == '__main__':
     unittest.main()
