3865dd0263d4bcd3c189c747a69e0b9716531924
Johan Herland <johan@herland.net>
2013-12-13 01:04:35 +0100

    Remove redundant tests

--- test_Manifest_from_walk.py
+++ test_Manifest_from_walk.py
@@ -46,30 +46,6 @@ class Test_Manifest_from_walk(unittest.TestCase):
 
     def test_files_at_many_levels(self):
         self.must_equal("files_at_many_levels.tar",
-
-    def must_equal_tar(self, tar_path, expect):
-        m = Manifest_from_tar(tar_path)
-        s = StringIO()
-        m.write(s)
-        self.assertEqual(s.getvalue(), expect)
-
-    def test_empty_tar(self):
-        self.must_equal_tar("empty.tar", "")
-
-    def test_single_file_tar(self):
-        self.must_equal_tar("single_file.tar", "foo\n")
-
-    def test_two_files_tar(self):
-        self.must_equal_tar("two_files.tar", "bar\nfoo\n")
-
-    def test_file_and_empty_subdir_tar(self):
-        self.must_equal_tar("file_and_empty_subdir.tar", "file\nsubdir\n")
-
-    def test_file_and_subdir_tar(self):
-        self.must_equal_tar("file_and_subdir.tar", "file\nsubdir\n\tfoo\n")
-
-    def test_files_at_many_levels_tar(self):
-        self.must_equal_tar("files_at_many_levels.tar",
             "bar\nbaz\n\tbar\n\tbaz\n\t\tbar\n\t\tbaz\n\t\tfoo\n\tfoo\nfoo\n")
 
 if __name__ == '__main__':
