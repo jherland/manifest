06ba864853445dfb14bbe30ad8495e648487e958
Johan Herland <johan@herland.net>
2014-02-18 20:39:00 +0100

    Test_Manifest_from_walk: Compare against expected dict, not output of .write()
    Use the fact that Manifest is a dict, to compare the result of .from_walk()
    directly against an expected dict, instead of calling .write() and comparing
    the resulting string against the expected string.
    
    This enables us to remove the StringIO dependency from this test.

--- test_Manifest_from_walk.py
+++ test_Manifest_from_walk.py
@@ -1,11 +1,4 @@
 import unittest
-try:
-    from cStringIO import StringIO # Most python2
-except ImportError:
-    try:
-        from StringIO import StringIO # Some python2
-    except ImportError:
-        from io import StringIO # python3
 
 from manifest import Manifest
 from test_utils import t_path, unpacked_tar, Manifest_from_walking_unpacked_tar
@@ -13,9 +6,8 @@ from test_utils import t_path, unpacked_tar, Manifest_from_walking_unpacked_tar
 class Test_Manifest_from_walk(unittest.TestCase):
 
     def must_equal(self, tar_path, expect):
-        s = StringIO()
-        Manifest_from_walking_unpacked_tar(tar_path).write(s)
-        self.assertEqual(s.getvalue(), expect)
+        m = Manifest_from_walking_unpacked_tar(tar_path)
+        self.assertEqual(m, expect)
 
     def test_missing_raises_ValueError(self):
         self.assertRaises(ValueError, Manifest.from_walk, t_path("missing"))
@@ -24,35 +16,41 @@ class Test_Manifest_from_walk(unittest.TestCase):
         self.assertRaises(ValueError, Manifest.from_walk, t_path("plain_file"))
 
     def test_empty_dir(self):
-        self.must_equal("empty.tar", "")
+        self.must_equal("empty.tar", {})
 
     def test_empty_dir_trailing_slash(self):
-        s = StringIO()
         with unpacked_tar("empty.tar") as d:
-            Manifest.from_walk(d + "/").write(s)
-        self.assertEqual(s.getvalue(), "")
+            m = Manifest.from_walk(d + "/")
+        self.assertEqual(m, {})
 
     def test_single_file(self):
-        self.must_equal("single_file.tar", "foo\n")
+        self.must_equal("single_file.tar", {"foo": {}})
 
     def test_two_files(self):
-        self.must_equal("two_files.tar", "bar\nfoo\n")
+        self.must_equal("two_files.tar", {"foo": {}, "bar": {}})
 
     def test_file_and_empty_subdir(self):
-        self.must_equal("file_and_empty_subdir.tar", "file\nsubdir\n")
+        self.must_equal("file_and_empty_subdir.tar", {"file": {}, "subdir": {}})
 
     def test_file_and_subdir(self):
-        self.must_equal("file_and_subdir.tar", "file\nsubdir\n\tfoo\n")
+        self.must_equal("file_and_subdir.tar",
+                        {"file": {}, "subdir": {"foo": {}}})
 
     def test_file_and_subdir_trailing_slash(self):
-        s = StringIO()
         with unpacked_tar("file_and_subdir.tar") as d:
-            Manifest.from_walk(d + "/").write(s)
-        self.assertEqual(s.getvalue(), "file\nsubdir\n\tfoo\n")
+            m = Manifest.from_walk(d + "/")
+        self.assertEqual(m, {"file": {}, "subdir": {"foo": {}}})
 
     def test_files_at_many_levels(self):
-        self.must_equal("files_at_many_levels.tar",
-            "bar\nbaz\n\tbar\n\tbaz\n\t\tbar\n\t\tbaz\n\t\tfoo\n\tfoo\nfoo\n")
+        self.must_equal("files_at_many_levels.tar",{
+            "foo": {},
+            "bar": {},
+            "baz": {
+                "foo": {},
+                "bar": {},
+                "baz": {"foo": {}, "bar": {}, "baz": {}}
+            }
+        })
 
 if __name__ == '__main__':
     unittest.main()
